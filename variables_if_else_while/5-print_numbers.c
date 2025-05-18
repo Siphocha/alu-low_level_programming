ce2a0c2650681eec7780902be8836e926d03f352
Siphocha <siphochakhala@gmail.com>
2025-05-18 11:30:51 +0200

    Can we do this again????

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Printing numbers from 10
+ *
+ *
+ */
+int main()
+{
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d", i);
+    }
+    printf("\n");
+    return (0);
+}
---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Same as last time but a bit diff
+ *
+ *
+ */
+int main()
+{
+    int i;
+
+    for (i = 48; i <= 57; i++)
+    {
+        putchar(i);
+    }
+    putchar('\n');
+    return (0);
+}
