dc7ec4e468eb68a8e953eb28127c9f1210c28549
Siphocha <siphochakhala@gmail.com>
2025-07-13 17:59:39 +0200

    slight change

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 /**
  * print_name - function that prints name
-zzz
+ * @name: parameter
+ * @f:function to be executed
  */
 void print_name(char *name, void (*f)(char *))
 {
